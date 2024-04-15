66042293fdfdd2f92efa2ec266b76727f8870179
Mario Geovani <marvanis@hotmail.com>
2024-04-15 10:36:56 -0600

    Agregamos archivos config

--- backend/config/config.php
+++ backend/config/config.php
@@ -1,1 +1,6 @@
 <?php
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', 'spartan1177');
+    define('DB_NAME', 'project_db');
+?>
---
+++ backend/db/Database.php
@@ -0,0 +1,20 @@
+<?php
+
+    require_once '../config/config.php';
+
+    class Database {
+        private $conn;
+
+        public function __construct()
+        {
+            $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+            if ($this->conn->connect_error){
+                die('Error de Conexion' . $this->conn->connect_error);
+            }
+        }
+
+        public function getConnection() {
+            return $this->conn;
+        }
+    }
